4accfc90580578000ad16d78bee4554fe910cb46
IgorDive <ihar.kazlovich@gmail.com>
2017-09-26 17:53:30 +0300

    modified class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+     constructor() {
+    	this._min = undefined;
+    	this._max = undefined;
+    	this._temp = undefined;
+    }
 
     setRange(min, max) {
-
+    	this._min = +min;
+    	this._max = +max;
     }
 
     guess() {
-
+    	this._temp = (this._max - this._min) / 2;
+    	return this._temp;
     }
 
     lower() {
-
+    	this._max = this._temp;
     }
 
     greater() {
-
+    	this._min = this._temp;
     }
 }
 
